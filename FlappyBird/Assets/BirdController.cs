bb99f7833e424a960ad66c34d0d2160183f1ff1a
Mateusz Pusty <mateusz.pusty@cdv.pl>
2024-05-09 12:59:14 +0200

    Moving pipes and jumping bird

--- FlappyBird/Assets/BirdController.cs
+++ FlappyBird/Assets/BirdController.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class BirdController : MonoBehaviour
 {
+    public Rigidbody2D rb2D;
+    public float JumpForce;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,10 @@ public class BirdController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Hello World per Update!");
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2D.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+
     }
 }
